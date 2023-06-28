a0ca979412f3dfef3b5eb440c6ae1e995b8aba7f
khalilSudo <khalilregui19@gmail.com>
2023-06-28 22:58:16 +0100

    done

--- _print_num.c
+++ _print_num.c
@@ -1,41 +1,31 @@
 /**
- * _print_num - Prints an integer to the console
- * @n: The integer to print
- * Return: The number of characters printed
+ * _print_num - Print a decimal integer.
+ * @n: The integer to print.
+ *
+ * Return: The number of characters printed.
  */
+#include <stdio.h>
+#include "main.h"
+
 int _print_num(int n)
 {
-    int i = 0;
-    int sign = 1;
-    char buffer[10];
+        int numPrintedChars = 0;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return (1);
-    }
+        if (n < 0)
+        {
+                /* Handle negative numbers */
+                _putchar('-');
+                n = -n;
+                numPrintedChars++;
+        }
 
-    if (n < 0)
-    {
-        sign = -1;
-        n = -n;
-    }
+        if (n / 10)
+        {
+                numPrintedChars += _print_num(n / 10);
+        }
 
-    while (n > 0)
-    {
-        buffer[i++] = n % 10 + '0';
-        n /= 10;
-    }
+        _putchar((n % 10) + '0');
+        numPrintedChars++;
 
-    if (sign == -1)
-    {
-        buffer[i++] = '-';
-    }
-
-    for (int j = i - 1; j >= 0; j--)
-    {
-        _putchar(buffer[j]);
-    }
-
-    return i;
+        return (numPrintedChars);
 }
